0607b32f545010ef5c9c4ef5ed5dffaac952ef76
JanGrzegorski <jan.grzegorski3@gmail.com>
2023-11-28 10:30:08 +0100

    Lekcja

---
+++ Zadanie/Zadanie/Zadanie.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int* tablica;
+    int ile = 5;
+    tablica = new int[ile];
+    
+    cout << "Dynamiczne adresy do przydzielania 5 ele = " << (int)tablica << endl;
+    cout << "Adres wlasny wskaznika tablica = " << (int)&tablica << endl;
+    cout << (int)&tablica[0] << endl;
+    cout << (int)&tablica[1] << endl;
+    cout << (int)&tablica[2] << endl;
+    cout << (int)&tablica[3] << endl;
+    cout << (int)&tablica[4] << endl;
+
+    cout << endl << endl;
+
+    for (int i = 0; i < ile; i++)
+    {
+        cout << (int)tablica << endl;
+        tablica++;
+    }
+
+    return 0;
+}
---
+++ Zadanie2/Zadanie2/Zadanie2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+    clock_t start, stop;
+    start = clock();
+
+    int los;
+    int ilosc;
+
+    cout << "Podaj ile elementow losujemy = "; cin >> ilosc;
+    int* tablica;
+    tablica = new int[ilosc];
+
+
+    for (int i = 0; i < ilosc; i++)
+    {
+        los = rand() % 100 + 1;
+        tablica[i] = los;
+    }
+
+    for (int i = 0; i < ilosc; i++)
+    {
+        cout << "Index nr " << i << " = " << tablica[i] << " Adres komorki: " << (int)&tablica[i] << endl;
+    }
+
+    stop = clock();
+
+    double czaswyswietlaniaaplikacji = (double)(stop - start) / ilosc / CLOCKS_PER_SEC;
+
+    cout << endl << czaswyswietlaniaaplikacji << endl;
+
+    delete[] tablica;
+
+    return EXIT_SUCCESS;
+}
